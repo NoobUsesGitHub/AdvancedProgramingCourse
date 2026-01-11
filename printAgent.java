34421457d99fa16d8798543b596c6369fd605114
Oz Ben Moshe <ozbm1000@Gmail.com>
2026-01-11 23:27:50 +0200

    "e"

--- printAgent.java
+++ printAgent.java
@@ -6,13 +6,16 @@ public class printAgent implements Agent{
     public printAgent(String n){this.input=n;
         TopicManagerSingleton.get().getTopic(n).subscribe(this);
     }
+    @Override
     public String getName(){return "";}
-    public void reset(){return;}
+    @Override
+    public void reset(){}
+    @Override
     public void callback(String topic, Message msg){
         if(this.input.equals(topic)){
             System.out.println(msg.asDouble);
         }
     }
-
-    public void close(){return;}
+    @Override
+    public void close(){}
 }
