ef324ebe024a2f23236ef97de6a9ef5712e3b830
Oz Ben Moshe <ozbm1000@Gmail.com>
2026-01-11 23:26:06 +0200

    done?

--- test/Graph.java
+++ test/Graph.java
@@ -21,39 +21,34 @@ public class Graph extends ArrayList<Node>{
     public void createFromTopics(){
         this.clear();
         Collection<Topic> t=TopicManagerSingleton.get().getTopics();
-        Map<Agent, Node> a = new HashMap<>();
-        //we have a shit ton of topics
-        // we have a shit ton of agents
-        // I want to run on all topics and Agents
-        // then create a node for every Agent and Topic, each attached to its other 
+        Map<String, Node> a = new HashMap<>();
         createTopicNodes(t.iterator(),a);//creates topics and attaches their subs
     }    
     
-    private void createTopicNodes(Iterator<Topic> i, Map<Agent,Node>a) {
-        HashSet<String> strSet=new HashSet<>();
+    private void createTopicNodes(Iterator<Topic> i, Map<String,Node>a) {
         while(i.hasNext()){
             Topic t=i.next();
             Node nT=new Node("T"+t.name);
             for(Agent s1:t.subs){
-                if(!a.containsKey(s1)){
-                    Node add=new Node("A"+s1.getName());
-                    a.put(s1,add);
+                String key = "A" + s1.getName();
+                if(!a.containsKey(key)){
+                    Node add=new Node(key);
+                    a.put(key,add);
                     nT.addEdge(add);
                     this.add(add);
-                    strSet.add(add.getName());
                 }else{
-                    nT.addEdge(a.get(s1));
+                    nT.addEdge(a.get(key));
                 }
             }
             for(Agent s2:t.pubs){
-                if(!strSet.contains("A"+s2.getName())){
-                    Node add=new Node("A"+s2.getName());
-                    a.put(s2,add);
+                String key = "A" + s2.getName();
+                if(!a.containsKey(key)){
+                    Node add=new Node(key);
+                    a.put(key,add);
                     add.addEdge(nT);
                     this.add(add);
-                    strSet.add(add.getName());
                 }else{
-                    a.get(s2).addEdge(nT);
+                    a.get(key).addEdge(nT);
                 }
             }
             this.add(nT);
