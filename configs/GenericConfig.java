aeb50d741bc50e03d731e96b64735b3e80c2c291
ayeletLus <ayeletlus3255@gmail.com>
2026-01-16 19:14:53 +0200

    this seems easy.... too easy

--- configs/GenericConfig.java
+++ configs/GenericConfig.java
@@ -1,8 +1,26 @@
 package configs;
 
-
+import graph.Agent;
 
 public class GenericConfig {
+    private String name;
+    private Agent[] agents;
 
+    public void setConfFile(){
+
+    }
+    public void create(){}
+    public String getName(){
+        return this.name;
+    }
+    public int getVersion(){
+        return 1;
+    }
+
+    public void close(){
+        for (Agent a: this.agents){
+            a.close();
+        }
+    }
 
 }
